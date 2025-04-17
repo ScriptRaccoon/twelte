ac469b2335ef9bde7e205138100253f0a400dfb6
Script Raccoon <scriptraccoon@gmail.com>
2025-04-17 23:10:26 +0200

    reply action

--- src/routes/post/[post_id]/+page.server.ts
+++ src/routes/post/[post_id]/+page.server.ts
@@ -1,6 +1,35 @@
-import type { PageServerLoad } from './$types'
+import { error, fail } from '@sveltejs/kit'
+import type { Actions, PageServerLoad } from './$types'
+import { post_content_schema } from '$lib/schemas'
+import { get_error_msg } from '$lib/utils'
+import { query } from '$lib/db'
 
 export const load: PageServerLoad = async (event) => {
 	const post_id = event.params.post_id
 	return { post_id }
+}
+
+export const actions: Actions = {
+	reply: async (event) => {
+		const user = event.locals.user
+		if (!user) error(401, 'Unauthorized')
+
+		const post_id = event.params.post_id
+
+		const form_data = await event.request.formData()
+		const content = form_data.get('content') as string | null
+
+		const { error: content_error } = post_content_schema.safeParse(content)
+
+		if (content_error) {
+			return fail(400, { error: get_error_msg(content_error), content })
+		}
+
+		const sql = 'INSERT INTO posts (author_id, content, parent_id) VALUES (?, ?, ?)'
+		const { err } = await query(sql, [user.id, content, post_id])
+
+		if (err) return fail(500, { error: 'Database error' })
+
+		return { success: true }
+	}
 }
--- src/routes/post/[post_id]/+page.svelte
+++ src/routes/post/[post_id]/+page.svelte
@@ -1,7 +1,27 @@
 <script lang="ts">
-	let { data } = $props()
+	import { enhance } from '$app/forms'
+
+	let { data, form } = $props()
 </script>
 
-<h1>Post Page for {data.post_id}</h1>
+<h1>Post Page for {data.post_id} (WIP)</h1>
 
-Here you can reply etc.
+{#if data.user}
+	<form action="?/reply" method="POST" use:enhance>
+		<input type="hidden" name="post_id" value={data.post_id} />
+		<textarea name="content" rows="4" cols="50" placeholder="Write your reply here..." required
+		></textarea>
+
+		<p>
+			<button type="submit">Reply</button>
+		</p>
+	</form>
+
+	{#if form?.error}
+		<p class="error">{form.error}</p>
+	{/if}
+
+	{#if form?.success}
+		<p>Reply has been sent!</p>
+	{/if}
+{/if}
