521130ea28794bb34fec8d6c42c1cc70cd6cda5b
Script Raccoon <scriptraccoon@gmail.com>
2025-04-17 23:40:04 +0200

    fetch and display replies

--- src/routes/post/[post_id]/+page.server.ts
+++ src/routes/post/[post_id]/+page.server.ts
@@ -4,9 +4,94 @@ import { post_content_schema } from '$lib/schemas'
 import { get_error_msg } from '$lib/utils'
 import { query } from '$lib/db'
 
+type PostDetail = {
+	id: number
+	author_id: number
+	author_handle: string
+	content: string
+	created_at: string
+	likes_count: number
+	liked_by_user: boolean
+}
+
 export const load: PageServerLoad = async (event) => {
+	const user = event.locals.user
 	const post_id = event.params.post_id
-	return { post_id }
+
+	const sql_post = `
+	SELECT
+		posts.id as id,
+		users.id as author_id,
+		users.handle as author_handle,
+		posts.content,
+		posts.created_at,
+		COALESCE(COUNT(likes.id), 0) as likes_count,
+		EXISTS (
+			SELECT 1
+			FROM likes
+			WHERE likes.post_id = posts.id AND likes.user_id = :user_id
+		) as liked_by_user
+	FROM
+		posts
+	INNER JOIN
+		users ON posts.author_id = users.id
+	LEFT JOIN
+		likes on posts.id = likes.post_id
+	WHERE
+		posts.id = :post_id
+		AND posts.parent_id IS NULL
+		AND posts.deleted = 0
+	GROUP BY
+		posts.id
+	`
+
+	const { rows: posts, success: success_post } = await query<PostDetail>(sql_post, {
+		user_id: user ? user.id : 0,
+		post_id
+	})
+
+	if (!success_post) error(500, 'Database error')
+
+	if (!posts.length) error(404, 'Post not found')
+
+	const post = posts[0]
+
+	const sql_replies = `
+	SELECT
+		posts.id as id,
+		users.id as author_id,
+		users.handle as author_handle,
+		posts.content,
+		posts.created_at,
+		COALESCE(COUNT(likes.id), 0) as likes_count,
+		EXISTS (
+			SELECT 1
+			FROM likes
+			WHERE likes.post_id = posts.id AND likes.user_id = :user_id
+		) as liked_by_user
+	FROM
+		posts
+	INNER JOIN
+		users ON posts.author_id = users.id
+	LEFT JOIN
+		likes on posts.id = likes.post_id
+	WHERE
+		posts.parent_id = :parent_id
+		AND posts.deleted = 0
+	GROUP BY
+		posts.id
+	ORDER BY
+		posts.created_at DESC
+	`
+
+	const { rows: replies, success: success_replies } = await query<PostDetail>(sql_replies, {
+		user_id: user ? user.id : 0,
+		parent_id: post.id
+	})
+
+	if (!success_replies) error(500, 'Database error')
+
+	return { post, replies }
 }
 
 export const actions: Actions = {
--- src/routes/post/[post_id]/+page.svelte
+++ src/routes/post/[post_id]/+page.svelte
@@ -2,13 +2,28 @@
 	import { enhance } from '$app/forms'
 
 	let { data, form } = $props()
+	let post = $derived(data.post)
+	let replies = $derived(data.replies)
 </script>
 
-<h1>Post Page for {data.post_id} (WIP)</h1>
+<h1>Post</h1>
+
+<!-- TODO: reuse Post component, make it more adjustable . . . -->
+
+<p>
+	<strong>
+		<a href="/profile/{post.author_handle}">@{post.author_handle}</a>
+	</strong>
+	&ndash;
+	{new Date(post.created_at).toLocaleString()}
+</p>
+
+<p>{post.content}</p>
+
+<h2>Your Reply</h2>
 
 {#if data.user}
 	<form action="?/reply" method="POST" use:enhance>
-		<input type="hidden" name="post_id" value={data.post_id} />
 		<textarea name="content" rows="4" cols="50" placeholder="Write your reply here..." required
 		></textarea>
 
@@ -24,4 +39,19 @@
 	{#if form?.success}
 		<p>Reply has been sent!</p>
 	{/if}
+{/if}
+
+{#if replies.length}
+	<h2>Replies</h2>
+	{#each replies as reply}
+		<p>
+			<strong>
+				<a href="/profile/{reply.author_handle}">@{reply.author_handle}</a>
+			</strong>
+			&ndash;
+			{new Date(reply.created_at).toLocaleString()}
+		</p>
+
+		<p>{reply.content}</p>
+	{/each}
 {/if}
