ba64fdfef246bb8d1787f09224856e24f0b38c3f
Script Raccoon <scriptraccoon@gmail.com>
2025-04-17 20:06:19 +0200

    sleep function for debugging purposes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,3 +3,7 @@ import type { ZodError } from 'zod'
 export function get_error_msg(err: ZodError) {
 	return err.errors[0].message
 }
+
+export function sleep(ms: number) {
+	return new Promise((resolve) => setTimeout(resolve, ms))
+}
