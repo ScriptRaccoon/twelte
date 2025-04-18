14b345e9cbebcf90806987505f6962c408e92cb4
Script Raccoon <scriptraccoon@gmail.com>
2025-04-18 18:49:05 +0200

    change password feature

--- src/routes/account/dashboard/+page.server.ts
+++ src/routes/account/dashboard/+page.server.ts
@@ -1,9 +1,10 @@
 import { error, redirect } from '@sveltejs/kit'
 import type { Actions, PageServerLoad } from '../$types'
 import { query_batched, query } from '$lib/db'
-import { bio_schema, display_name_schema, email_schema } from '$lib/schemas'
+import { bio_schema, display_name_schema, email_schema, password_schema } from '$lib/schemas'
 import { fail } from '@sveltejs/kit'
 import { get_error_msg } from '$lib/utils'
+import bcrypt from 'bcryptjs'
 
 export const load: PageServerLoad = async (event) => {
 	const user = event.locals.user
@@ -49,13 +50,14 @@ export const actions: Actions = {
 		const bio = (form_data.get('bio') ?? '') as string
 
 		const { error: email_error } = email_schema.safeParse(email)
-		if (email_error) return fail(400, { error: get_error_msg(email_error) })
+		if (email_error) return fail(400, { action: 'edit', error: get_error_msg(email_error) })
 
 		const { error: display_name_error } = display_name_schema.safeParse(display_name)
-		if (display_name_error) return fail(400, { error: get_error_msg(display_name_error) })
+		if (display_name_error)
+			return fail(400, { action: 'edit', error: get_error_msg(display_name_error) })
 
 		const { error: bio_error } = bio_schema.safeParse(bio)
-		if (bio_error) return fail(400, { error: get_error_msg(bio_error) })
+		if (bio_error) return fail(400, { action: 'edit', error: get_error_msg(bio_error) })
 
 		const sql_email = 'UPDATE users SET email = ? WHERE id = ?'
 		const sql_profile = 'UPDATE profiles SET display_name = ?, bio = ? WHERE user_id = ?'
@@ -67,10 +69,10 @@ export const actions: Actions = {
 
 		if (err) {
 			if (err.code === 'SQLITE_CONSTRAINT_UNIQUE') {
-				return fail(400, { error: 'Email already exists' })
+				return fail(400, { action: 'edit', error: 'Email already exists' })
 			}
 
-			return fail(500, { error: 'Database error' })
+			return fail(500, { action: 'edit', error: 'Database error' })
 		}
 
 		redirect(302, '/account/dashboard?code=updated')
@@ -83,10 +85,55 @@ export const actions: Actions = {
 		const sql = 'DELETE FROM users WHERE id = ?'
 		const { err } = await query(sql, [user.id])
 
-		if (err) return fail(500, { error: 'Database error' })
+		if (err) return fail(500, { action: 'delete', error: 'Database error' })
 
 		event.cookies.delete('jwt', { path: '/' })
 
 		throw redirect(302, '/')
+	},
+
+	password: async (event) => {
+		const user = event.locals.user
+		if (!user) redirect(302, '/account/login')
+
+		const form_data = await event.request.formData()
+		const old_password = form_data.get('old_password') as string | null
+		const new_password = form_data.get('new_password') as string | null
+
+		if (old_password === new_password)
+			return fail(400, {
+				action: 'password',
+				error: 'New password must be different from old password'
+			})
+
+		const { error: parse_error } = password_schema.safeParse(new_password)
+		if (parse_error) return fail(400, { action: 'password', error: get_error_msg(parse_error) })
+
+		const { rows } = await query<{ password_hash: string }>(
+			'SELECT password_hash FROM users WHERE id = ?',
+			[user.id]
+		)
+
+		if (!rows) return fail(500, { action: 'password', error: 'Database error' })
+		if (!rows.length)
+			return fail(401, { action: 'password', error: 'Old Password is incorrect' })
+
+		const { password_hash } = rows[0]
+
+		const password_is_correct = await bcrypt.compare(old_password!, password_hash)
+
+		if (!password_is_correct)
+			return fail(401, { action: 'password', error: 'Old Password is incorrect' })
+
+		const new_password_hash = await bcrypt.hash(new_password!, 10)
+
+		const { success } = await query('UPDATE users SET password_hash = ? WHERE id = ?', [
+			new_password_hash,
+			user.id
+		])
+
+		if (!success) return fail(500, { action: 'password', error: 'Database error' })
+
+		return { action: 'password', message: 'Password updated' }
 	}
 }
--- src/routes/account/dashboard/+page.svelte
+++ src/routes/account/dashboard/+page.svelte
@@ -19,58 +19,90 @@
 	</form>
 </header>
 
-<p class="handle" aria-label="handle">@{account.handle}</p>
+<section aria-label="basic information">
+	<p class="handle" aria-label="handle">@{account.handle}</p>
 
-<form action="?/edit" method="POST" use:enhance class="form">
-	<div class="input-group">
-		<label for="email">Email</label>
-		<input type="email" id="email" name="email" value={account.email} />
-	</div>
+	<form action="?/edit" method="POST" use:enhance class="form">
+		<div class="input-group">
+			<label for="email">Email</label>
+			<input type="email" id="email" name="email" value={account.email} />
+		</div>
 
-	<div class="input-group">
-		<label for="display_name">Display Name</label>
-		<input type="text" id="display_name" name="display_name" value={account.display_name} />
-	</div>
+		<div class="input-group">
+			<label for="display_name">Display Name</label>
+			<input type="text" id="display_name" name="display_name" value={account.display_name} />
+		</div>
 
-	<div class="input-group">
-		<label for="bio">Bio</label>
-		<textarea id="bio" name="bio" rows="3">{account.bio}</textarea>
-	</div>
+		<div class="input-group">
+			<label for="bio">Bio</label>
+			<textarea id="bio" name="bio" rows="3">{account.bio}</textarea>
+		</div>
 
-	<button class="button" type="submit">Update Profile</button>
-</form>
+		<button class="button" type="submit">Update Profile</button>
+	</form>
 
-{#if form?.error}
-	<p class="error">{form.error}</p>
-{/if}
-
-{#if !form && data.message}
-	<InfoMessage>
-		{data.message}
-	</InfoMessage>
-{/if}
-
-<h3>Danger Zone</h3>
-
-<form action="?/delete" method="POST" use:enhance class="form">
-	{#if confirm_deletion}
-		<button class="button" type="submit">Yes! Delete account</button>
-	{:else}
-		<button
-			class="button"
-			type="button"
-			onclick={() => {
-				confirm_deletion = true
-			}}
-		>
-			Delete account
-		</button>
+	{#if form?.error && form.action == 'edit'}
+		<p class="error">{form.error}</p>
 	{/if}
-</form>
 
-{#if confirm_deletion}
-	<p class="error">Are you sure? This cannot be undone!</p>
-{/if}
+	{#if !form && data.message}
+		<InfoMessage>
+			{data.message}
+		</InfoMessage>
+	{/if}
+</section>
+
+<section>
+	<h3>Security</h3>
+
+	<form action="?/password" method="POST" use:enhance class="form">
+		<div class="input-group">
+			<label for="old_password">Old Password</label>
+			<input type="password" id="old_password" name="old_password" required />
+		</div>
+
+		<div class="input-group">
+			<label for="new_password">New Password</label>
+			<input type="password" id="new_password" name="new_password" required />
+		</div>
+
+		<button class="button" type="submit">Change Password</button>
+	</form>
+
+	{#if form?.error && form.action == 'password'}
+		<p class="error">{form.error}</p>
+	{/if}
+
+	{#if form?.message && form.action === 'password'}
+		<InfoMessage>
+			{form.message}
+		</InfoMessage>
+	{/if}
+</section>
+
+<section>
+	<h3>Danger Zone</h3>
+
+	<form action="?/delete" method="POST" use:enhance class="form">
+		{#if confirm_deletion}
+			<button class="button" type="submit">Yes! Delete account</button>
+		{:else}
+			<button
+				class="button"
+				type="button"
+				onclick={() => {
+					confirm_deletion = true
+				}}
+			>
+				Delete account
+			</button>
+		{/if}
+	</form>
+
+	{#if confirm_deletion}
+		<p class="error">Are you sure? This cannot be undone!</p>
+	{/if}
+</section>
 
 <style>
 	.handle {
@@ -80,5 +112,9 @@
 	header {
 		display: flex;
 		justify-content: space-between;
+	}
+
+	section {
+		margin-bottom: 2rem;
 	}
 </style>
