3a837c7092288f453b53b05b558628cd4bfad953
Script Raccoon <scriptraccoon@gmail.com>
2025-04-18 19:32:31 +0200

    add footer with link

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,15 @@
+<footer>
+	<a href="https://github.com/ScriptRaccoon/twelte" target="_blank">GitHub</a>
+</footer>
+
+<style>
+	footer {
+		margin-top: auto;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding-block: 1rem;
+		gap: 1rem;
+		color: var(--secondary-font-color);
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import './app.css'
 	import Header from '$lib/components/Header.svelte'
+	import Footer from '$lib/components/Footer.svelte'
 
 	let { data, children } = $props()
 </script>
@@ -8,5 +9,20 @@
 <div class="wrapper">
 	<Header user={data.user} />
 
-	{@render children?.()}
+	<div>
+		{@render children?.()}
+	</div>
+
+	<Footer />
 </div>
+
+<style>
+	.wrapper {
+		max-width: 42rem;
+		margin: 0 auto;
+		padding-inline: 0.75rem;
+		display: grid;
+		min-height: 100vh;
+		grid-template-rows: auto auto 1fr;
+	}
+</style>
