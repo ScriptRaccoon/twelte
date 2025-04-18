f47f3155d4aa38dde1b7136ff191db8a5544a388
Script Raccoon <scriptraccoon@gmail.com>
2025-04-18 11:28:35 +0200

    style create account page, add info message component

---
+++ src/lib/components/InfoMessage.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+
+	import Fa from 'svelte-fa'
+
+	let { children } = $props()
+</script>
+
+<div>
+	<span>
+		<Fa icon={faInfoCircle} />
+	</span>
+
+	{@render children?.()}
+</div>
+
+<style>
+	div {
+		margin-block: 1.5rem;
+		background-color: rgb(21, 94, 21);
+		padding: 0.5rem 1rem;
+		border-radius: 0.25rem;
+	}
+
+	span {
+		margin-right: 0.25rem;
+	}
+</style>
--- src/routes/account/create/+page.svelte
+++ src/routes/account/create/+page.svelte
@@ -1,33 +1,54 @@
 <script lang="ts">
 	import { enhance } from '$app/forms'
+	import InfoMessage from '$lib/components/InfoMessage.svelte'
+	import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+	import Fa from 'svelte-fa'
 
 	let { form } = $props()
 </script>
 
 <h2>Create an account</h2>
 
-<form method="POST" action="?/register" use:enhance>
-	<label for="email">Email</label>
-	<input type="email" id="email" name="email" required value={form?.email} />
+<form method="POST" action="?/register" use:enhance class="form">
+	<div class="input-group">
+		<label for="email">Email</label>
+		<input
+			type="email"
+			id="email"
+			name="email"
+			required
+			value={form?.email}
+			readonly={form?.success}
+		/>
+	</div>
+	<div class="input-group">
+		<label for="handle">Handle*</label>
+		<input
+			type="text"
+			id="handle"
+			name="handle"
+			required
+			value={form?.handle}
+			readonly={form?.success}
+		/>
+	</div>
+	<div class="input-group">
+		<label for="password">Password</label>
+		<input type="password" id="password" name="password" required readonly={form?.success} />
+	</div>
 
-	<label for="handle">Handle</label>
-	<input type="text" id="handle" name="handle" required value={form?.handle} />
-
-	<label for="password">Password</label>
-	<input type="password" id="password" name="password" required />
-
-	<button type="submit">Create Account</button>
-
-	<p>Notice that the handle cannot be changed anymore after registration.</p>
+	<button class="button" type="submit" disabled={form?.success}>Create Account</button>
 </form>
+
+<p>*Notice that the handle cannot be changed anymore after registration.</p>
 
 {#if form?.error}
 	<p class="error">{form.error}</p>
 {/if}
 
 {#if form?.success}
-	<p>
-		User has been created. You can now
+	<InfoMessage>
+		Account has been created. You can now
 		<a href="/account/login">login</a>.
-	</p>
+	</InfoMessage>
 {/if}
