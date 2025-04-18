f224b11a14476b7c8d24a650574b47dc8f4779da
Script Raccoon <scriptraccoon@gmail.com>
2025-04-18 23:28:26 +0200

    add two indices

--- database/seed.sql
+++ database/seed.sql
@@ -50,6 +50,8 @@ CREATE TABLE IF NOT EXISTS posts (
     FOREIGN KEY (parent_id) REFERENCES posts (id) ON DELETE CASCADE
 );
 
+CREATE INDEX idx_posts_created_at ON posts (created_at);
+
 CREATE TABLE IF NOT EXISTS likes (
     user_id INTEGER NOT NULL,
     post_id INTEGER NOT NULL,
@@ -58,6 +60,8 @@ CREATE TABLE IF NOT EXISTS likes (
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
     FOREIGN KEY (post_id) REFERENCES posts (id) ON DELETE CASCADE
 );
+
+CREATE INDEX idx_likes_post_id ON likes (post_id);
 
 CREATE TABLE IF NOT EXISTS follows (
     follower_id INTEGER NOT NULL,
