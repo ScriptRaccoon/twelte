b99fb5a713b45e4f7c99ad2140ff2b5c39cbe62c
Script Raccoon <scriptraccoon@gmail.com>
2025-04-20 03:38:47 +0200

    loading animation for two forms

--- src/routes/account/create/+page.svelte
+++ src/routes/account/create/+page.svelte
@@ -3,6 +3,17 @@
 	import Message from '$lib/components/Message.svelte'
 
 	let { form } = $props()
+
+	let submitting = $state(false)
+
+	function handle_submit(e: SubmitEvent) {
+		if (submitting) e.preventDefault()
+		submitting = true
+	}
+
+	$effect(() => {
+		if (form) submitting = false
+	})
 </script>
 
 <svelte:head>
@@ -11,7 +22,7 @@
 
 <h2>Create an account</h2>
 
-<form method="POST" action="?/register" use:enhance class="form">
+<form method="POST" action="?/register" use:enhance class="form" onsubmit={handle_submit}>
 	<div class="input-group">
 		<label for="email">Email</label>
 		<input
@@ -39,18 +50,29 @@
 		<input type="password" id="password" name="password" required readonly={form?.success} />
 	</div>
 
-	<button class="button" type="submit" disabled={form?.success}>Create Account</button>
+	<button
+		class="button"
+		type="submit"
+		disabled={submitting || form?.success}
+		class:loading={submitting}
+	>
+		{#if submitting}
+			Creating account...
+		{:else}
+			Create account
+		{/if}
+	</button>
 </form>
 
 <p>*Notice that the handle cannot be changed anymore after registration.</p>
 
-{#if form?.error}
+{#if form?.error && !submitting}
 	<Message type="error">
 		{form.error}
 	</Message>
 {/if}
 
-{#if form?.success}
+{#if form?.success && !submitting}
 	<Message type="success">
 		Account has been created. We have sent you a verification email. Please click on the link in
 		the email to verify your account. After that, you can <a href="/account/login">login</a>.
--- src/routes/account/login/+page.svelte
+++ src/routes/account/login/+page.svelte
@@ -6,6 +6,17 @@
 	let { form } = $props()
 
 	const code = page.url.searchParams.get('code')
+
+	let submitting = $state(false)
+
+	function handle_submit(e: SubmitEvent) {
+		if (submitting) e.preventDefault()
+		submitting = true
+	}
+
+	$effect(() => {
+		if (form) submitting = false
+	})
 </script>
 
 <svelte:head>
@@ -18,7 +29,7 @@
 	<Message type="success">Your email has been verified. You can now login.</Message>
 {/if}
 
-<form method="POST" use:enhance class="form">
+<form method="POST" use:enhance class="form" onsubmit={handle_submit}>
 	<div class="input-group">
 		<label for="handle">Handle</label>
 		<input type="text" id="handle" name="handle" required value={form?.handle} />
@@ -29,10 +40,16 @@
 		<input type="password" id="password" name="password" required />
 	</div>
 
-	<button class="button" type="submit">Login</button>
+	<button class="button" type="submit" disabled={submitting} class:loading={submitting}>
+		{#if submitting}
+			Login...
+		{:else}
+			Login
+		{/if}
+	</button>
 </form>
 
-{#if form?.error}
+{#if form?.error && !submitting}
 	<Message type="error">
 		{form.error}
 	</Message>
