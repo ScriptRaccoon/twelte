9f42f79a23f4a30fbeaa29acc081c4c93c3ee4b6
Script Raccoon <scriptraccoon@gmail.com>
2025-04-23 13:17:08 +0200

    remove samples

--- database/migrations/007-sample-follows.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO
-    follows (follower_id, followed_id)
-VALUES
-    (1, 2),
-    (1, 3),
-    (2, 3);
--- database/migrations/013-sample-settings.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO
-    SETTINGS (user_id)
-VALUES
-    (1),
-    (2),
-    (3);
